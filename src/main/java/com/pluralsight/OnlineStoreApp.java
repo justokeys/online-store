00bdb321a5258c66caaf4f9e14e9e8f2e510363a
JustinK <productivekeys@gmail.com>
2026-04-24 15:59:40 -0500

    finished display Products option in main menu , working on the adding and then checkout menus

--- src/main/java/com/pluralsight/OnlineStoreApp.java
+++ src/main/java/com/pluralsight/OnlineStoreApp.java
@@ -6,16 +6,17 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.lang.invoke.SwitchPoint;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 
-public class OnlineStoreApp  {
+public class OnlineStoreApp {
     static Scanner thescanner = new Scanner(System.in);
-    static ArrayList<Products>forSale = new ArrayList<>();
-    static ArrayList<Products>sold = new ArrayList<>();
+    static ArrayList<Products> forSale = new ArrayList<>();
+    static ArrayList<Products> sold = new ArrayList<>();
 
     public static void main(String[] args) {
 
-       getInventory();
+        getInventory();
 
         System.out.println("                ----- MacroCenter -----");
         System.out.println("" +
@@ -34,59 +35,61 @@ public class OnlineStoreApp  {
         boolean appRunning = true;
 
 
-        while (appRunning){
+        while (appRunning) {
             System.out.println("""
-        --- Select an Option ---
-        1 - Display Products
-        2 - Display CART
-        3 - EXIT (Close program)
-        
-        """);
+                    --- Select an Option ---
+                    1 - Display Products
+                    2 - Display CART
+                    3 - EXIT (Close program)
+                    
+                    """);
 
             int userInput = thescanner.nextInt();
 
-            switch (userInput){
+            switch (userInput) {
 
                 case 1:
                     displayProducts();
                     break;
                 case 2:
-                   //displayCart();
+                    //displayCart();
                     break;
                 case 3:
                     System.out.println("Thank you visiting The MacroCenter");
                     System.out.println(
-                            "⣿⣿⠿⠿⠿⠿⣿⣷⣂⠄⠄⠄⠄⠄⠄⠈⢷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n" +
-                            "⣷⡾⠯⠉⠉⠉⠉⠚⠑⠄⡀⠄⠄⠄⠄⠄⠈⠻⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n" +
-                            "⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⡀⠄⠄⠄⠄⠄⠄⠄⠄⠉⠻⣿⣿⣿⣿⣿⣿⣿\n" +
-                            "⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⠎⠄⠄⣀⡀⠄⠄⠄⠄⠄⠄⠄⠘⠋⠉⠉⠉⠉⠭\n" +
-                            "⡀⠄⠄⠄⠄⠄⠄⠄⠄⡇⠄⣠⣾⣳⠁⠄⠄⢺⡆⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄\n" +
-                            "⣿⣷⡦⠄⠄⠄⠄⠄⢠⠃⢰⣿⣯⣿⡁⢔⡒⣶⣯⡄⢀⢄⡄⠄⠄⠄⠄⠄⣀\n" +
-                            "⠓⠄⠄⠄⠄⠄⠸⠄⢀⣤⢘⣿⣿⣷⣷⣿⠛⣾⣿⣿⣆⠾⣷⠄⠄⠄⠄⢀⣀\n" +
-                            "⠄⠄⠄⠄⠄⠄⠄⠑⢘⣿⢰⡟⣿⣿⣷⣫⣭⣿⣾⣿⣿⣴⠏⠄⠄⢀⣺⣿⣿\n" +
-                            "⣿⣿⣿⣿⣷⠶⠄⠄⠄⠹⣮⣹⡘⠛⠿⣫⣾⣿⣿⣿⡇⠑⢤⣶⣿⣿⣿⣿⣿\n" +
-                            "⣿⣿⣿⣯⣤⣤⣤⣤⣀⣀⡹⣿⣿⣷⣯⣽⣿⣿⡿⣋⣴⡀⠈⣿⣿⣿⣿⣿⣿\n" +
-                            "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣝⡻⢿⣿⡿⠋⡒⣾⣿⣧⢰⢿⣿⣿⣿⣿⣿");
+                                    "⣿⣿⠿⠿⠿⠿⣿⣷⣂⠄⠄⠄⠄⠄⠄⠈⢷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n" + "" +
+                                    "⣷⡾⠯⠉⠉⠉⠉⠚⠑⠄⡀⠄⠄⠄⠄⠄⠈⠻⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n" +
+                                    "⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⡀⠄⠄⠄⠄⠄⠄⠄⠄⠉⠻⣿⣿⣿⣿⣿⣿⣿\n" +
+                                    "⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⠎⠄⠄⣀⡀⠄⠄⠄⠄⠄⠄⠄⠘⠋⠉⠉⠉⠉⠭\n" +
+                                    "⡀⠄⠄⠄⠄⠄⠄⠄⠄⡇⠄⣠⣾⣳⠁⠄⠄⢺⡆⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄\n" +
+                                    "⣿⣷⡦⠄⠄⠄⠄⠄⢠⠃⢰⣿⣯⣿⡁⢔⡒⣶⣯⡄⢀⢄⡄⠄⠄⠄⠄⠄⣀\n" +
+                                    "⠓⠄⠄⠄⠄⠄⠸⠄⢀⣤⢘⣿⣿⣷⣷⣿⠛⣾⣿⣿⣆⠾⣷⠄⠄⠄⠄⢀⣀\n" +
+                                    "⠄⠄⠄⠄⠄⠄⠄⠑⢘⣿⢰⡟⣿⣿⣷⣫⣭⣿⣾⣿⣿⣴⠏⠄⠄⢀⣺⣿⣿\n" +
+                                    "⣿⣿⣿⣿⣷⠶⠄⠄⠄⠹⣮⣹⡘⠛⠿⣫⣾⣿⣿⣿⡇⠑⢤⣶⣿⣿⣿⣿⣿\n" +
+                                    "⣿⣿⣿⣯⣤⣤⣤⣤⣀⣀⡹⣿⣿⣷⣯⣽⣿⣿⡿⣋⣴⡀⠈⣿⣿⣿⣿⣿⣿\n" +
+                                    "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣝⡻⢿⣿⡿⠋⡒⣾⣿⣧⢰⢿⣿⣿⣿⣿⣿");
                     appRunning = false;
-               // default:
-                    //System.out.println("Invalid option. Please try again.");
-                   // break;
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+                    break;
 
             }
 
         }
 
     }
+
     // Retrives data from the csv file, parses and formats that data
     public static void getInventory() {
 
-        try{
+        try {
             FileReader fileReader = new FileReader("src/main/resources/products (1).csv");
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             bufferedReader.readLine();
 
             String line;
-            while ((line = bufferedReader.readLine()) != null){
+            while ((line = bufferedReader.readLine()) != null) {
 
                 String[] productsAll = line.split("\\|");
 
@@ -95,7 +98,7 @@ public class OnlineStoreApp  {
                 double price = Double.parseDouble(productsAll[2]);
                 String department = productsAll[3];
 
-                Products allProductsFS = new Products(sku,productName,price,department);
+                Products allProductsFS = new Products(sku, productName, price, department);
 
                 forSale.add(allProductsFS);
 
@@ -112,8 +115,9 @@ public class OnlineStoreApp  {
 
 
     }
+
     // Displays all items we currently for sale
-    public static void displayProducts(){
+    public static void displayProducts() {
 
         boolean appRunning = true;
 
@@ -140,14 +144,185 @@ public class OnlineStoreApp  {
                             ======================================================================
                             """
             );
+            System.out.println("""
+                    
+                    =========================================
+                             PRODUCT INVENTORY MENU
+                    =========================================
+                    What would you like to do next?
+                    
+                    1 - Search or Filter Products
+                    2 - Add a Product to Cart
+                    3 - Go Back to Main Menu
+                    =========================================
+                    Enter command:\s""");
+            int userInput = thescanner.nextInt();
+
+            switch (userInput) {
+
+                case 1:
+                    searchAndFilter();
+                    break;
+                case 2:
+                     addToCart();
+                    break;
+
+                case 3:
+                    appRunning = false;
+                    break;
 
 
-
+            }
 
 
         }
+
+    }
+
+    public static void searchAndFilter() {
+        boolean appRunning = true;
+
+        while (appRunning) {
+            System.out.println("1 - Search by SKU");
+            System.out.println("2 - Search by Price range");
+            System.out.println("3 - Search by Department");
+            System.out.println("4 - Search by Product Name");
+            System.out.println("5 - Exit back to main menu");
+            int userInput = thescanner.nextInt();
+            thescanner.nextLine();
+            switch (userInput) {
+
+                case 1:
+                    System.out.print("Enter SKU: ");
+                    String sku = thescanner.nextLine();
+                    for (Products currentProduct : forSale) {
+
+                        if (sku.equalsIgnoreCase(currentProduct.getsKU())) {
+
+                            System.out.printf("SKU: %s | %s | %s | %.2f\n", currentProduct.getsKU(), currentProduct.getProductName(), currentProduct.getDepartment(), currentProduct.getPrice());
+
+                        }
+                    }
+                    break;
+                case 2:
+                    sortbyPrice();
+                    break;
+                case 3:
+                    System.out.print("Enter Department: ");
+                    String department = thescanner.nextLine();
+                    for (Products currentProduct : forSale) {
+
+                        if (department.equalsIgnoreCase(currentProduct.getDepartment())) {
+
+                            System.out.printf("SKU: %s | %s | %s | %.2f\n", currentProduct.getsKU(), currentProduct.getProductName(), currentProduct.getDepartment(), currentProduct.getPrice());
+
+                        }
+                    }
+                    break;
+                case 4:
+                    System.out.print("Enter Product Name: ");
+                    String nameP = thescanner.nextLine();
+                    for (Products currentProduct : forSale) {
+
+                        if (nameP.equalsIgnoreCase(currentProduct.getProductName())) {
+
+                            System.out.printf("SKU: %s | %s | %s | %.2f\n", currentProduct.getsKU(), currentProduct.getProductName(), currentProduct.getDepartment(), currentProduct.getPrice());
+
+                        }
+                    }
+                    break;
+                case 5:
+                    appRunning = false;
+                    break;
+
+
+            }
+        }
+
+
+
+
+
+
+    }
+
+    public static void sortbyPrice(){
+       boolean appRunning = true;
+
+        while (appRunning)
+
+
+        System.out.println("Enter price range minimum");
+        double min = thescanner.nextDouble();
+        System.out.println("Enter price range maximum");
+        double max = thescanner.nextDouble();
+        thescanner.nextLine();
+
+        for (Products product : forSale) {
+
+            if (product.getPrice() >= min && product.getPrice() <= max){
+                System.out.printf("SKU: %s | %s | %s | %.2f\n", product.getsKU(), product.getProductName(), product.getDepartment(), product.getPrice());
+
+            }
+        }
+
+
+
+    }
+
+    public static void addToCart(){
+        System.out.print(
+                """
+                        ======================================================================
+                           *                                                       *
+                        ======================================================================
+                         Price      | Product Name                   | Department     | SKU
+                        ----------------------------------------------------------------------
+                        """
+        );
+
+        for (Products product : forSale) {
+            System.out.printf("SKU: %s | %s | %s | %.2f\n", product.getsKU(), product.getProductName(), product.getDepartment(), product.getPrice());
+
+        }
+        System.out.println(
+                """
+                        ----------------------------------------------------------------------                   
+                        ======================================================================
+                        """
+        );
+        System.out.println("""
+                    
+                    =========================================
+                             PRODUCT INVENTORY MENU
+                    =========================================
+                    What would you like to do next?
+                    
+                    1 - Search or Filter Products
+                    2 - Add a Product to Cart
+                    3 - Go Back to Main Menu
+                    =========================================
+                    Enter command:\s""");
+        int userInput = thescanner.nextInt();
+
+
+
+
+
+
+
+
+
     }
 
 
 
-}
+
+
+
+
+    }
+
+
+
+
